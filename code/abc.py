16da6ce2769e0efc414dd4bb0b11449d0a897e07
pawanbajeli <singhbajeli7088@gmail.com>
2023-02-12 13:51:42 +0530

    first commit

---
+++ code/abc.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+i=620
+while(i<800):
+  path="./fetched_data/file_"+str(i)+".csv"
+  df = pd.read_csv(path)
+  matrix1=df.corr().round(2)
+  a=plt.figure(figsize=(19,16))
+  sns.heatmap(matrix1,annot=True)
+  plt.savefig('./images/heatmap_'+str(i)+'.png')
+  plt.close()
+  print(i, flush=True)
+  i=i+1;
---
+++ code/f.py
@@ -0,0 +1,18 @@
+# import lib
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+i=1800
+while(i<2000):
+  path="./fetched_data/file_"+str(i)+".csv"
+  df = pd.read_csv(path)
+  matrix1=df.corr().round(2)
+  a=plt.figure(figsize=(19,16))
+  sns.heatmap(matrix1,annot=True)
+  plt.savefig('./images/heatmap_'+str(i)+'.png')
+  plt.close()
+  print(i, flush=True)
+  i=i+1;
